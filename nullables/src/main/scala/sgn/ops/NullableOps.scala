3a1b740fd455e56cecdea9cc7fbf8ec5441b9d6f
Nicholas Yip <nicholasyip@obceit.cc>
2019-05-20 01:34:39 +0900

    throw new NoSuchElementException("Nullable.get")

--- nullables/src/main/scala/sgn/ops/NullableOps.scala
+++ nullables/src/main/scala/sgn/ops/NullableOps.scala
@@ -9,7 +9,7 @@ class NullableOps[A] private[sgn] (private val value: Any) extends AnyVal {
   def get: A = {
     val r =
       value match {
-        case BoxedNull => throw new NoSuchElementException()
+        case BoxedNull => throw new NoSuchElementException("Nullable.get")
         case LiftedNullness(n) => n
         case _ => value
       }
